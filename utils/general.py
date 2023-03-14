21c71f177a6533dffb91feb91d9160a2b6d88a59
Johannes Schuck <jojoschuck@gmail.com>
2023-03-14 13:19:09 +0100

    look in runs dir by default

--- utils/general.py
+++ utils/general.py
@@ -47,7 +47,7 @@ def init_seeds(seed=0):
     init_torch_seeds(seed)
 
 
-def get_latest_run(search_dir='.'):
+def get_latest_run(search_dir='runs'):
     # Return path to most recent 'last.pt' in /runs (i.e. to --resume from)
     last_list = glob.glob(f'{search_dir}/**/last*.pt', recursive=True)
     return max(last_list, key=os.path.getctime) if last_list else ''
